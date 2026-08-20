b8bc7e0f8eb3ce6ae482c49d3afa7150dd60e478
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-20 16:08:36 +0000

    allow production frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://camp-sec-system-pvfg.onrender.com",
+        "https://campus-security-system-57t7.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
