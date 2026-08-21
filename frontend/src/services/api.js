5b415bd72634da72886745b4001b31ce948a324b
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-21 11:06:51 +0000

    fix: connect frontend to deployed backend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://camp-sec-backend-new.onrender.com";
+
+
 export async function getIncidents() {
   const response = await fetch(`${API_URL}/incidents`);
 
